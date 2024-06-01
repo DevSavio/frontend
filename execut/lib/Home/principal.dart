74dc79bf6f74a5e62973ef5a965b43203a5539ef
DevSavio <develop.savio@gmail.com>
2024-06-01 16:02:31 -0300

    Editado para que os elementos da pagina tenha comportamento dinamico

--- execut/lib/Home/principal.dart
+++ execut/lib/Home/principal.dart
@@ -7,96 +7,104 @@ class Principal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: sizeW * 0.8571425,
-          height: sizeH * 1.0,
-          decoration: const BoxDecoration(color: Color.fromARGB(255, 250, 143, 143)),
-          child: Stack(
-            children: [
-              const Positioned(
-                left: 29,
-                top: 40,
-                child: Text(
-                  'ULTIMAS OBRAS',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontFamily: 'Nunito Sans',
-                    fontWeight: FontWeight.w700,
+      body: Container(
+        decoration:
+            const BoxDecoration(color: Color.fromARGB(255, 250, 143, 143)),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const Text(
+                        'Execut Construções e Reformas',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color(0xFF202224),
+                          fontSize: 36,
+                          fontFamily: 'Nunito Sans',
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: -0.11,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Wrap(
+                        alignment: WrapAlignment.spaceBetween,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: _buildInfoCard(
+                              title: 'Total Mão de Obra',
+                              value: '40,689',
+                              icon: Icons.home,
+                              color: Color(0xFFE5E5E5),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: _buildInfoCard(
+                              title: 'Total Mão de Obra',
+                              value: '40,689',
+                              icon: Icons.home,
+                              color: Color(0xFFE5E5E5),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: _buildInfoCard(
+                              title: 'Total Mão de Obra',
+                              value: '40,689',
+                              icon: Icons.home,
+                              color: Color(0xFFE5E5E5),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: _buildInfoCard(
+                              title: 'Total Mão de Obra',
+                              value: '40,689',
+                              icon: Icons.home,
+                              color: Color(0xFFE5E5E5),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Positioned(
+                        left: 29,
+                        top: 474,
+                        child: _buildOrderList(),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              const Positioned(
-                left: 339,
-                top: 91,
-                child: Text(
-                  'Execut Construções e Reformas',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF202224),
-                    fontSize: 36,
-                    fontFamily: 'Nunito Sans',
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: -0.11,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 29,
-                top: 200,
-                child: _buildInfoCard(
-                  title: 'Total Mão de Obra',
-                  value: '40,689',
-                  imageUrl: "https://via.placeholder.com/60x60",
-                ),
-              ),
-              Positioned(
-                left: 321,
-                top: 200,
-                child: _buildInfoCard(
-                  title: 'Total Pagamentos',
-                  value: '\$89,000',
-                  imageUrl: "https://via.placeholder.com/60x60",
-                ),
-              ),
-              Positioned(
-                left: 613,
-                top: 200,
-                child: _buildInfoCard(
-                  title: 'Total Recursos',
-                  value: '2040',
-                  imageUrl: "https://via.placeholder.com/60x60",
-                ),
-              ),
-              Positioned(
-                left: 905,
-                top: 200,
-                child: _buildInfoCard(
-                  title: 'Total Obras',
-                  value: '10293',
-                  imageUrl: "https://via.placeholder.com/60x60",
-                ),
-              ),
-              Positioned(
-                left: 29,
-                top: 474,
-                child: _buildOrderList(),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Widget _buildInfoCard(
-      {required String title,
-      required String value,
-      required String imageUrl}) {
+  Widget _buildInfoCard({
+    required String title,
+    required String value,
+    required IconData icon,
+    required Color color,
+  }) {
     return Container(
-      width: sizeW * 0.1857142,
-      height: sizeH * 0.161,
+      height: 200,
+      width: 300,
       decoration: BoxDecoration(
         color: const Color(0xFFF5F4F4),
         borderRadius: BorderRadius.circular(14),
@@ -110,9 +118,10 @@ class Principal extends StatelessWidget {
         ],
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: sizeW * 0.0114285, vertical: sizeH * 0.016),
+        padding: const EdgeInsets.all(25),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text(
               title,
@@ -123,23 +132,31 @@ class Principal extends StatelessWidget {
                 fontWeight: FontWeight.w600,
               ),
             ),
-            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: color,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Icon(
+                    icon,
+                    color: const Color(0xFF000000),
+                    size: 38,
+                  ),
+                ),
+              ],
+            ),
             Text(
               value,
               style: const TextStyle(
                 color: Colors.black,
                 fontSize: 28,
                 fontFamily: 'Nunito Sans',
-                fontWeight: FontWeight.w700,
+                fontWeight: FontWeight.w400,
                 letterSpacing: 1,
-              ),
-            ),
-            const Spacer(),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Opacity(
-                opacity: 0.30,
-                child: Image.network(imageUrl, width: sizeW * 0.0428571, height: sizeH * 0.06),
               ),
             ),
           ],
@@ -182,7 +199,8 @@ class Principal extends StatelessWidget {
 
   Widget _buildOrderHeader() {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: sizeW * 0.0114285, vertical: sizeH * 0.016),
+      padding: const EdgeInsets.symmetric(
+          horizontal: sizeW * 0.0114285, vertical: sizeH * 0.016),
       decoration: BoxDecoration(
         color: const Color(0xFFF1F3F9),
         borderRadius: BorderRadius.circular(12),
@@ -221,7 +239,8 @@ class Principal extends StatelessWidget {
     required Color statusColor,
   }) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: sizeW * 0.0114285, vertical: sizeH * 0.016),
+      padding: const EdgeInsets.symmetric(
+          horizontal: sizeW * 0.0114285, vertical: sizeH * 0.016),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -250,7 +269,8 @@ class Principal extends StatelessWidget {
 
   Widget _buildStatusBox(String status, Color color) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: sizeW * 0.00857128, vertical: sizeH * 0.008),
+      padding: const EdgeInsets.symmetric(
+          horizontal: sizeW * 0.00857128, vertical: sizeH * 0.008),
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(13.5),
