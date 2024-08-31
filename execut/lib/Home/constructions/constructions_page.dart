f3e1b4f65a400b9b3e7c661756d69c602cee60be
DevSavio <develop.savio@gmail.com>
2024-08-31 18:39:22 -0300

    Retirado codigp sem uso da tela

--- execut/lib/Home/constructions/constructions_page.dart
+++ execut/lib/Home/constructions/constructions_page.dart
@@ -237,25 +237,6 @@ class _ConstructionsPageState extends State<ConstructionsPage> {
             ),
           ),
         );
-        // AlertDialog(
-        //   title: const Text('AlertDialog Title'),
-        //   content: const SingleChildScrollView(
-        //     child: ListBody(
-        //       children: <Widget>[
-        //         Text('This is a demo alert dialog.'),
-        //         Text('Would you like to approve of this message?'),
-        //       ],
-        //     ),
-        //   ),
-        //   actions: <Widget>[
-        //     TextButton(
-        //       child: const Text('Approve'),
-        //       onPressed: () {
-        //         Navigator.of(context).pop();
-        //       },
-        //     ),
-        //   ],
-        // );
       },
     );
   }
